a3c64ad56fd1b16a520fd4a44c68887abe5438bd
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2022-10-20 19:42:47 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,6 +5,9 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        
+        public List<float> CalculateStatistics(List<float> numbers)
+        {
+            return numbers;
+        }
     }
 }
