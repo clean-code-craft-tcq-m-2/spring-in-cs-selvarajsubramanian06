39354a673b868df94002d1bc9ea9daad742d3ea1
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2022-10-20 19:08:06 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -3,6 +3,8 @@ using Xunit;
 using Statistics;
 using System.Collections.Generic;
 
+
+
 namespace Statistics.Test
 {
     public class StatsUnitTest
@@ -11,15 +13,12 @@ namespace Statistics.Test
         public void ReportsAverageMinMax()
         {
             var statsComputer = new StatsComputer();
-            /*var computedStats = statsComputer.CalculateStatistics(
-                new List<float>{1.5, 8.9, 3.2, 4.5});
+            var computedStats = statsComputer.CalculateStatistics(
+                new List<float>{1.5f, 8.9f, 3.2f, 4.5f});
             float epsilon = 0.001F;
             Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);*/
-            Assert.True(true);
-            Assert.True(true);
-            Assert.True(true);
+            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
         }
         [Fact]
         public void ReportsNaNForEmptyInput()
@@ -34,18 +33,22 @@ namespace Statistics.Test
         [Fact]
         public void RaisesAlertsIfMaxIsMoreThanThreshold()
         {
-            /*var emailAlert = new EmailAlert();
-            var ledAlert = new LEDAlert();
-            IAlerter[] alerters = {emailAlert, ledAlert};
+            var emailAlert = new EmailAlert();
+            var ledAlert = new EmailAlert();
+            //IAlerter[] alerters = {emailAlert, ledAlert};
 
-            const float maxThreshold = 10.2;
-            var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<float>{0.2, 11.9, 4.3, 8.5});
 
-            Assert.True(emailAlert.emailSent);
-            Assert.True(ledAlert.ledGlows);*/
-            Assert.True(true);
-            Assert.True(true);
+
+           //const float maxThreshold = 10.2f;
+            //var statsAlerter = new StatsAlerter(maxThreshold, alerters);
+            //statsAlerter.checkAndAlert(new List<float>{0.2f, 11.9f, 4.3f, 8.5f});
+
+
+
+
+
+           Assert.True(emailAlert.emailSent);
+            Assert.True(ledAlert.ledGlows);
         }
     }
 }
