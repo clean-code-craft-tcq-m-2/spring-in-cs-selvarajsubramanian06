0871d0cba7fefd587dcb8b05c1507bb068d67f94
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2022-10-20 19:37:54 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,24 +5,6 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public float max, min, average = 0;
-        public List<float> CalculateStatistics(List<float> numbers) {
-            /*this.min = numbers[0];
-            this.max = numbers[0];
-            foreach (var item in numbers)
-            {
-                this.average += item;
-                if (min > item)
-                {
-                    this.min = item;
-                }
-                if (max < item)
-                {
-                    this.max = item;
-                }
-            }
-            average /= numbers.Count;*/
-            return numbers;
-        }
+        
     }
 }
